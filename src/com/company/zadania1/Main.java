f29550fa3029fb19de341f979324b073a3d6bf08
Marek Kuchnowski <73584685+kiorun@users.noreply.github.com>
2020-11-06 02:56:57 +0100

    Add files via upload

---
+++ src/com/company/zadania1/Main.java
@@ -0,0 +1,25 @@
+package zadania1;
+public class Main {
+    public static void main(String[] args) {
+
+
+    uczen Marek = new uczen();
+
+
+
+    Marek.info(18,"Kuchnowski","Marek");
+
+        System.out.println(Marek.imie);
+         System.out.println(Marek.nazwisko);
+        System.out.println(Marek.wiek +"lat");
+
+
+        System.out.println(" zadanie 2 ");
+        Matma d = new Matma();
+ d.inc(3.14, 2.71);
+ d.nahh(3.14,2.71);
+d.troj(3.14,2.71,3);
+
+        }
+    }
+
---
+++ src/com/company/zadania1/uczen.java
@@ -0,0 +1,24 @@
+package zadania1;
+
+public class uczen {
+
+        String imie;
+        String nazwisko;
+        int wiek;
+
+
+
+
+
+
+
+
+        void info(int wiek,  String nazwisko,String imie) {
+            this.imie = imie;
+                this.wiek = wiek;
+                this.nazwisko = nazwisko;
+        }
+
+}
+
+
